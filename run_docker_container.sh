605466bd576eb2055d50d18c107d3a7f0bb18424
Christopher Sahadeo <chris@virtanatech.com>
2023-03-22 13:36:57 -0400

    Updated readme & ports on dockerfile

--- run_docker_container.sh
+++ run_docker_container.sh
@@ -6,7 +6,6 @@ docker run -it --rm \
   --net=host \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   --env=DISPLAY \
-  -p 3030:3030 \
   --env=QT_X11_NO_MITSHM=1 \
   -v $(pwd):/root/rust-warp-react \
   -w /root/rust-warp-react \
