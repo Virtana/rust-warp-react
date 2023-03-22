1052c169d88bda14e74007f70258bb16bd0dd654
Christopher Sahadeo <chris@virtanatech.com>
2023-03-22 15:49:29 -0400

    v1 works! Done.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,19 @@ import { Button, Flex, } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 function App() {
+
+  const handleClick = async () => {
+    const response = await fetch('/name', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+
+    const result = await response.json();
+    console.log(JSON.stringify(result, null, 4));
+  }
+
   return (
     <Flex
       direction={{ base: 'column', large: 'row' }}
@@ -12,7 +25,9 @@ function App() {
       <Flex justifyContent="space-between" direction="column">
         <Button
           variation="primary"
-          onClick={() => alert('See console output...')}
+          onClick={() => {
+            handleClick()
+            alert('See console output...')}}
         >
           Hi, my name is.. 
         </Button>
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,79 @@
 #![deny(warnings)]
-use warp::Filter;
+use warp::{Filter};
+
+// use warp::{sse::Event, Filter};
+
+// use futures_util::StreamExt;
+
+// use std::convert::Infallible;
+// use std::time::Duration;
+
+// use tokio::time::interval;
+
+// use tokio_stream::wrappers::IntervalStream;
+
+// fn print_my_name_wrapper() -> Result<Event, Infallible> {
+//     print_my_name();
+//     Ok(warp::sse::Event::default().data("Slim Shady!"))
+// }
+
+fn print_my_name() {
+    println!("Slim Shady!");
+}
 
 #[tokio::main]
 async fn main() {
     // Match any request and return hello world!
-    let routes = warp::any().map(|| "Hello, World!");
+    // let routes = warp::any().map(|| "Hello, World!");
+    // let routes = warp::path::end().and(warp::get()).map(|| {
+    //     // create server event source
+    //     let interval = interval(Duration::from_secs(1));
+    //     let stream = IntervalStream::new(interval);
+    //     let event_stream = stream.map(move |_| {
+    //         println!("Hello");
+    //         print_my_name_wrapper()s
+    //     });
+    //     // reply using server-sent events
+    //     warp::sse::reply(event_stream)
+    // });
+
+
+
+    // GET / -> index html
+    let index = warp::path::end().map(|| {
+        warp::http::Response::builder()
+            .header("content-type", "text/html; charset=utf-8")
+            .body(INDEX_HTML)
+    });
+
+    // let post = warp::path("name").and(warp::post()).map(|| {
+    //     // create server event source
+    //     print_my_name();
+    //     // reply using server-sent events
+    //     warp::reply()
+    // });
+
+    let post = warp::path!("name").map(|| {
+      print_my_name();
+      // format!("Test")
+      warp::reply::json(&"Slim Shady!")
+    });
+
+    let routes = index.or(post);
+
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
+
+
+static INDEX_HTML: &str = r#"
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Warp</title>
+    </head>
+    <body>
+        <h1>warp</h1>
+    </body>
+</html>
+"#;
